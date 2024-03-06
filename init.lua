0ca95b5db2d982a3df0ebb9a67bf1e31e57d0198
Hunter Dyar <bloop@hdyar.com>
2024-03-05 23:54:04 -0500

    tests and buffers

--- init.lua
+++ init.lua
@@ -381,7 +381,7 @@ require('lazy').setup {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = '[F]ind exiting [b]uffers (or space space)' })
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to telescope to change theme, layout, etc.
--- lua/features.lua
+++ lua/features.lua
@@ -1,4 +1,18 @@
 return {
   { 'simrat39/symbols-outline.nvim', opts = {} },
+  --the first line being the first line of the function you are in.
   { 'nvim-treesitter/nvim-treesitter-context' },
+  --Splash screen
+  {
+    'echasnovski/mini.starter',
+    version = false,
+    config = {
+      header = 'ahoy, bloops',
+    },
+  },
+  --{ 'gelguy/wilder.nvim', config = {
+  --modes = { ':' },
+  --} },
+  --{ 'smolck/command-completion.nvim', setup = { border = nil, use_matchfuzzy = true } },
+  { 'kassio/neoterm' },
 }
